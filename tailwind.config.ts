412c0470eff47cf982d0509845dd5ad3201e2e41
Naman <namansoin000@gmail.com>
2025-08-14 17:55:34 +0000

    change the display font in other pages aswell

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Inter', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        headline: ['Silkscreen', 'sans-serif'],
         code: ['monospace'],
         silkscreen: ['Silkscreen', 'sans-serif'],
       },
